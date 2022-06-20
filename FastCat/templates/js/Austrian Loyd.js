bc8a629a6ae360a0ac1516bd3b1892af1703cc69
Kostas Petrakis <petrakis1@gmail.com>
2022-06-20 18:28:06 +0300

    Update Austrian Loyd.js

--- FastCat/templates/js/Austrian Loyd.js
+++ FastCat/templates/js/Austrian Loyd.js
@@ -1,10 +1,3 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-///////////////////SAM mikroi pinakes//////////////////////////////////////////
 
 function createSubTable(parentTableName, subTableName, x, y, row, startCol, endCol) {
     $(".sub_table").remove();
